512efe8bec8d5744cfd447eb6dd16bef60a0413f
Ахмед Магомедов <immortalkil4oy@gmail.com>
2025-01-14 00:41:55 +0300

    create brain-even file

---
+++ bin/brain-even.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+"use strict";
+
+import readLineSync from "readline-sync";
+
+console.log("Welcome to the Brain Games!");
+
+const firstName = readLineSync.question("May I have your name? ");
+console.log(`Hello, ${firstName}!`);
+
+console.log(`Answer "yes" if the number is even, otherwise answer "no"`);
+
+const honestNum = (firstName) => {
+  let randomNum;
+  let userAnswer;
+  let correctCount = 0;
+
+  while (correctCount < 3) {
+    randomNum = Math.floor(Math.random() * 100);
+
+    console.log(`Qustion: ${randomNum}`);
+    userAnswer = readLineSync.question("Your answer: ");
+    const correctAnswer = randomNum % 2 === 0 ? "yes" : "no";
+
+    if (userAnswer !== "yes" && userAnswer !== "no") {
+      console.log("Wrong");
+      break;
+    }
+
+    if (userAnswer === correctAnswer) {
+      correctCount++;
+      console.log("Correct!");
+    } else {
+      console.log(
+        `"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}"`
+      );
+      console.log(`Let's try again, ${firstName}`);
+      break;
+    }
+    console.log(`Congratulations, ${firstName}!`);
+  }
+};
+
+honestNum(firstName);
