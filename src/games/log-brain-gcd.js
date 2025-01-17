18f36bf3e3244c073f7c959d04bcd30baf6d693b
Ахмед Магомедов <immortalkil4oy@gmail.com>
2025-01-17 22:06:52 +0300

    feat: лобавление логики игры brain-gcd

---
+++ src/games/log-brain-gcd.js
@@ -0,0 +1,32 @@
+"use strict";
+
+import { randomTask } from "../index.js";
+import randomNum from "../random-num.js";
+
+const gcd = (paif) => {
+  const arrPaif = paif.split(" ");
+  console.log(paif);
+  let a = arrPaif[0];
+  let b = arrPaif[1];
+  while (b !== 0) {
+    let temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+};
+
+const randomPaif = (min, max) => {
+  const a = randomNum(min, max);
+  const b = randomNum(min, max);
+  const result = `${a} ${b}`;
+  return result;
+};
+
+const textQust = "Find the greates common divisor of given numbers.";
+
+const runBrainGcd = () => {
+  return randomTask(textQust, randomPaif, 10, 50, gcd);
+};
+
+export default runBrainGcd;
