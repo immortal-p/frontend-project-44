2800ca30abf962d0e3ceb6179b5507b761917afb
Ахмед Магомедов <immortalkil4oy@gmail.com>
2025-01-19 00:36:04 +0300

    refactor  function

--- src/random-num.js
+++ src/random-num.js
@@ -1,7 +1,6 @@
 "use strict";
 
-const randomNum = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+const randomNum = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
 
 export default randomNum;
