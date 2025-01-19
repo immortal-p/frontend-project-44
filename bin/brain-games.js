42aba866efdba0f88951382a3c970af55c9e2588
Ахмед Магомедов <immortalkil4oy@gmail.com>
2025-01-19 23:08:21 +0300

    refactor: transferring game logic to another file

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 "use strict";
-import { getName } from "../src/cli.js";
+import runBrainGames from "../src/games/log-brain-games.js";
 
-console.log("Welcome to the Brain Games!");
-
-const firstName = getName();
-console.log(`Hello ${firstName}`);
+runBrainGames();
---
+++ src/games/log-brain-games.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import { getName } from "../cli.js";
+
+const runBrainGames = () => {
+  console.log("Welcome to the Brain Games!");
+
+  const firstName = getName();
+  console.log(`Hello ${firstName}`);
+};
+
+export default runBrainGames;
