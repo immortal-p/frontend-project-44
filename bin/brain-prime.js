c4d9c1cc036086c9ddfc21e18e879c89c182a53e
Ахмед Магомедов <immortalkil4oy@gmail.com>
2025-01-19 23:04:04 +0300

    feat: create a new game and implement its logic in separate files

---
+++ bin/brain-prime.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+"use strict";
+
+import runBrainPrime from "../src/games/log-brain-prime.js";
+
+runBrainPrime();
---
+++ src/games/log-brain-prime.js
@@ -0,0 +1,25 @@
+"use strict";
+
+import randomNum from "../random-num.js";
+import { randomTask } from "../index.js";
+
+function randomN(min, max) {
+  return randomNum(min, max);
+}
+
+const isPrime = (num) => {
+  if (num <= 1) return "no";
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return "no";
+  }
+
+  return "yes";
+};
+
+const textQust = `Answer "yes" if given number is prime. Otherwise answer "no"`;
+
+const runBrainPrime = () => {
+  randomTask(textQust, randomN, 0, 30, isPrime);
+};
+
+export default runBrainPrime;
