95da7916d16a9e3f5887e4e81220fe44f63682fa
Ахмед Магомедов <immortalkil4oy@gmail.com>
2025-01-22 21:07:24 +0300

    refactor: code

--- src/games/log-brain-even.js
+++ src/games/log-brain-even.js
@@ -1,11 +1,11 @@
 import generateRandomNum from '../random-num.js';
 import playBrainGame from '../index.js';
 
+const isEven = (num) => num % 2 === 0;
 const getCorrectAnswer = (num) => {
-    const isEven = (num) => num % 2 === 0;
-    const expectedAnswer = isEven(num) ? 'yes' : 'no';
-    return expectedAnswer
-}
+  const expectedAnswer = isEven(num) ? 'yes' : 'no';
+  return expectedAnswer;
+};
 const description = 'Answer "yes" if the number is even, otherwise answer "no".';
 
 const runBrainEven = () => playBrainGame(description, generateRandomNum, 0, 50, getCorrectAnswer);
